f1022cb6008d585a25318c8ea7d7fb5e75e7ca04
Carlos Simon <git@csimon.dev>
2020-09-25 18:51:50 +0200

    Create post-install.sh

---
+++ post-install.sh
@@ -0,0 +1,3 @@
+yay -Syu archlinux-themes-sddm firefox-bin zettlr-bin todoist-electron brave-bin thunderbird docker docker-composer
+yay -Syu whatsapp-nativefier telegram-desktop-bin visual-studio-code-bin albert-lite espanso-bin
+
