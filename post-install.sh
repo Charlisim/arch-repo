ca017045e5536a8eff349a0ec769ca02133a11c0
Carlos Simon <git@csimon.dev>
2020-09-26 14:53:47 +0200

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,5 +1,5 @@
 yay -Syu archlinux-themes-sddm firefox-bin zettlr-bin todoist-electron brave-bin thunderbird docker docker-composer synology-drive vorta zoom
-yay -Syu whatsapp-nativefier telegram-desktop-bin visual-studio-code-bin albert-lite espanso-bin vlc 1password-bin timeshift pulse
+yay -Syu whatsapp-nativefier telegram-desktop-bin visual-studio-code-bin albert-lite espanso-bin vlc 1password-bin timeshift pulse rsync jdk pamac-aur
 mkdir -p ~/Descargas
 mkdir -p ~/Documentos
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
