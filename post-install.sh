0903cb8b33cadbc74ad1d0f759afb9f323927b37
Carlos Simón <git@csimon.dev>
2020-10-11 10:23:55 +0200

    Unify for manjaro

--- post-install.sh
+++ post-install.sh
@@ -2,10 +2,9 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 gpg --keyserver keyserver.ubuntu.com --recv-keys 4773BD5E130D1D45
-yay -Syu archlinux-themes-sddm firefox zettlr-bin todoist-electron brave-bin thunderbird docker synology-drive vorta zoom
-yay -Syu whatsapp-nativefier telegram-desktop-bin visual-studio-code-bin albert-lite espanso-bin vlc 1password-bin
-yay -Syu timeshift pulse rsync jdk pamac-aur lshw sof bind-tools mtr socat htop iotop openbsd-netcat strace tcpdump whois
-yay -Syu yakuake kdiff3 latte-dock spotify 
+yay -Syu firefox zettlr-bin todoist-electron brave-bin thunderbird docker synology-drive vorta zoom
+yay -Syu whatsapp-nativefier telegram-desktop-bin visual-studio-code-bin albert-lite espanso-bin vlc 1password-bin timeshift pulse rsync jdk pamac-aur lshw sof bind-tools mtr socat htop iotop openbsd-netcat strace tcpdump whois kdiff3 noto-fonts noto-fonts-cjk noto-fonts-emoji  xorg-fonts-misc ttf-font-awesome
+yay -Syu kdiff3 latte-dock spotify 
 
 yay -Syu iftop dstat wl-clipboard brightnessctl playerctl swayshot udiskie j4-dmenu-desktop bemenu mako
 yay -Syu qt5-wayland redshift python-gobject bluez bluez-utils pulseaudio pulseaudio-alsa
